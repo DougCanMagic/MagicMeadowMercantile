b951b693ddad346f7cd8fb5b5c6010f69b00b323
Doug Emerson <dugsbugs@gmail.com>
2022-12-31 15:18:21 -0600

    second commit (ready to code)

--- magic_meadow_farm/pages/index.js
+++ magic_meadow_farm/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         width={100}
         height={100}
       />
-      <h1>Welcome to Magic Meadow Farm</h1>
+      <h1>Magic Meadow Farm</h1>
     </>
   );
 }
