787157dfa27be374a6f1469c8334e70b86f87d48
Doug Emerson <57976623+DougCanMagic@users.noreply.github.com>
2023-01-26 21:48:16 -0600

    reverting to previous

--- magic_meadow_farm/components/Navbar.js
+++ magic_meadow_farm/components/Navbar.js
@@ -1,16 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import React, { useState } from 'react';
 
-const MENU_LIST = [
-	{ text: 'Home', href: '/' },
-	{ text: 'About Us', href: '/about' },
-	{ text: 'Mercantile', href: '/mercantile' },
-];
-const Navbar = () => {
-	const [navActive, setNavActive] = useState(null);
-	const [activeIdx, setActiveIdx] = useState(-1);
-
+export default function Navbar() {
 	return (
 		<header>
 			<div className='nav-header'>
@@ -18,8 +9,8 @@ const Navbar = () => {
 					className='main-nav-image'
 					src='/mmf_icon_alpha_nowords_1.png'
 					alt='Magic Meadow Farm logo'
-					width={65}
-					height={65}
+					width={85}
+					height={85}
 				/>
 				<h2>Magic Meadow Farm</h2>
 				<div>X</div>
@@ -32,6 +23,4 @@ const Navbar = () => {
 			</div>
 		</header>
 	);
-};
-
-export default Navbar;
+}
