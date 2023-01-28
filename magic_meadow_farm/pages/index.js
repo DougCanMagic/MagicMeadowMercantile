fa38008a159305c8fcf916976cf6cc7725fb72c9
Doug Emerson <57976623+DougCanMagic@users.noreply.github.com>
2023-01-27 22:44:43 -0600

    main and p-tag to homepage

--- magic_meadow_farm/pages/index.js
+++ magic_meadow_farm/pages/index.js
@@ -4,7 +4,18 @@ export default function Home() {
 	return (
 		<>
 			<Navbar />
-			<h1>Welcome to Magic Meadow Farm and Mercantile</h1>
+			<main>
+				<h1>Welcome to Magic Meadow Farm and Mercantile</h1>
+				<p>
+					Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem quod
+					officiis odio quis rerum molestiae harum alias, vel eveniet? Nobis
+					iste beatae quam quo veniam quae sapiente quisquam ipsum soluta. Lorem
+					ipsum dolor sit amet consectetur, adipisicing elit. Iste, asperiores
+					voluptatibus. Illo quod vero at odit corporis sint placeat dolor
+					veritatis, provident iusto harum doloribus autem accusamus suscipit
+					eligendi ducimus.
+				</p>
+			</main>
 		</>
 	);
 }
